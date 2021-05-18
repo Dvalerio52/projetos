d28a39c88fd5aab84050c4cf53fb94293ef38ecb
Douglas Valerio <douglas.valerio@xdata.com.br>
2021-05-18 16:08:16 -0300

    Codigo de estudos

---
+++ autmacao receita federal.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import time
+import socket
+from selenium.webdriver.chrome import service
+import unittest
+
+ip = "ip numbers"
+port = "port numbers "
+
+driver = webdriver.Remote(
+    desired_capabilities=webdriver.DesiredCapabilities.CHROME,
+    command_executor='http://ip numbers:port numbers/wd/hub')
+
+
+
+#headless
+#options = webdriver.ChromeOptions()
+#options.add_argument("--headless")
+#driver = webdriver.Chrome(chrome_options=options)
+driver.get("https://google.com")
+
+
+user_name = "###########"
+password = "#########"
+#driver = webdriver.Chrome()
+driver.get("https://servicos.receita.fazenda.gov.br/servicos/cpf/consultasituacao/consultapublica.asp")
+element = driver.find_element_by_id("txtCPF")
+element.send_keys(user_name)
+element = driver.find_element_by_id("txtDataNascimento")
+element.send_keys(password)
+WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']")))
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='recaptcha-checkbox goog-inline-block recaptcha-checkbox-unchecked rc-anchor-checkbox']/div[@class='recaptcha-checkbox-border']"))).click()
+element = driver.find_element_by_class_name("recaptcha-checkbox-checkmark")
+element = driver.find_element_by_id("id_submit").click()
+element.add_argument_option("excludeSwitches", ["enable-automation"])
+element.add_argument_option('useAutomationExtension', False)
+data_sitekey = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CLASS_NAME, "g-recaptcha"))).get_attribute("data-sitekey")
+print(data_sitekey)
+
+
+
+
+
+
+element.close()
